f2d000ae4a8675ca793498dd394416c08c3081ec
xiaweiyi723 <xiaweiyi723@gmail.com>
2026-09-07 07:40:16 +0800

    feat: export repository Q&A API

--- src/repopilot/__init__.py
+++ src/repopilot/__init__.py
@@ -1,16 +1,20 @@
 """RepoPilot package."""
 
 from .chunks import CodeChunk, chunk_repository, chunk_source
+from .qa import Citation, RepositoryAnswer, answer_repository
 from .scanner import RepositoryInventory, SourceFile, scan_repository
 from .symbols import CodeSymbol, PythonModuleMap, build_python_symbol_map
 
 __all__ = [
     "CodeChunk",
+    "Citation",
     "CodeSymbol",
     "PythonModuleMap",
     "RepositoryInventory",
+    "RepositoryAnswer",
     "SourceFile",
     "build_python_symbol_map",
+    "answer_repository",
     "chunk_repository",
     "chunk_source",
     "scan_repository",
